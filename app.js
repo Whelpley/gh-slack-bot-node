907c4cd1baf6fd3ee71f99abd570be3295682e92
Whelpley <whelpley@gmail.com>
2016-09-27 14:57:46 -0400

    Debug

--- app.js
+++ app.js
@@ -9,20 +9,23 @@ var port = process.env.PORT || 3000;
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 // test route
 app.get('/', function (req, res) { res.status(200).send('Hello world!') });
 
-//hellobot
+// hellobot
 app.post('/hello', hellobot);
 
 // dicebot
 app.post('/roll', dicebot);
 
-// error handler
+
+// basic error handler
 app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(400).send(err.message);
 });
+
 
 app.listen(port, function () {
   console.log('Slack bot listening on port ' + port);
